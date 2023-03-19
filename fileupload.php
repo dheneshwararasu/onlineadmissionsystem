f3d69850c631440ee20e6aec6401d0e7c14811b0
dheneshwararasu <42833926+dheneshwararasu@users.noreply.github.com>
2023-03-20 02:11:39 +0800

    Update fileupload.php

--- fileupload.php
+++ fileupload.php
@@ -1,56 +1,84 @@
-
 <?php
 session_start();
-$sp=mysqli_connect("localhost","root","","oas");
-         if($sp->connect_errno){
-                echo "Error <br/>".$sp->error;
+$sp = mysqli_connect("localhost", "root", "", "oas");
+
+if ($sp->connect_errno) {
+    echo "Error <br/>".$sp->error;
 }
 
-$picpath="studentpic/";
-$docpath="studentdoc/";
-$proofpath="studentproof/";
-$id=$_SESSION['user'];
-if(isset($_POST['fpicup']))
+function validateFile($file, $allowedTypes, $maxSize)
 {
-$picpath=$picpath.$_FILES['fpic']['name'];
-$docpath1=$docpath.$_FILES['ftndoc']['name'];     
-$docpath2=$docpath.$_FILES['ftcdoc']['name']; 
-$docpath3=$docpath.$_FILES['fdmdoc']['name']; 
-$docpath4=$docpath.$_FILES['fdcdoc']['name'];     
-$proofpath1=$proofpath.$_FILES['fide']['name']; 
-$proofpath2=$proofpath.$_FILES['fsig']['name']; 
+    // Check for file upload errors
+    if ($file['error'] !== UPLOAD_ERR_OK) {
+        return "Error uploading file";
+    }
 
-if(move_uploaded_file($_FILES['fpic']['tmp_name'],$picpath)
-  && move_uploaded_file($_FILES['ftndoc']['tmp_name'],$docpath1)
-  && move_uploaded_file($_FILES['ftcdoc']['tmp_name'],$docpath2)
-  && move_uploaded_file($_FILES['fdmdoc']['tmp_name'],$docpath3)
-  && move_uploaded_file($_FILES['fdcdoc']['tmp_name'],$docpath4)
-  && move_uploaded_file($_FILES['fide']['tmp_name'],$proofpath1)
-  && move_uploaded_file($_FILES['fsig']['tmp_name'],$proofpath2))
-{
+    // Check if file types are allowed
+    $fileType = strtolower(pathinfo($file['name'], PATHINFO_EXTENSION));
+    if (!in_array($fileType, $allowedTypes)) {
+        return "Invalid file type";
+    }
 
-$img=$_FILES['fpic']['name'];
-$img1=$_FILES['ftndoc']['name'];
-$img2=$_FILES['ftcdoc']['name'];
-$img3=$_FILES['fdmdoc']['name'];
-$img4=$_FILES['fdcdoc']['name'];
-$img5=$_FILES['fide']['name'];
-$img6=$_FILES['fsig']['name'];
+    // Check if file sizes are within the allowed limit
+    if ($file['size'] > $maxSize) {
+        return "File size exceeds limit";
+    }
 
+    return "";
+}
 
-$query="insert into t_userdoc (s_id,s_pic,s_tenmarkpic,s_tencerpic,
-    s_twdmarkpic, s_twdcerpic, s_idprfpic, s_sigpic) values 
-    ('$id','$img','$img1','$img2','$img3','$img4','$img5','$img6')";
-        if($sp->query($query)){
-     echo "Inserted to DB ";    
-    }else
+$picpath = "studentpic/";
+$docpath = "studentdoc/";
+$proofpath = "studentproof/";
+$id = $_SESSION['user'];
+
+if (isset($_POST['fpicup'])) {
+    $allowedImageTypes = array('image/jpeg', 'image/png', 'image/gif');
+    $allowedDocumentTypes = array('application/pdf', 'application/msword', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document');
+    $allowedProofTypes = array('image/jpeg', 'image/png', 'image/gif', 'application/pdf');
+    $maxFileSize = 10 * 1024 * 1024; // 10 MB
+
+    $picValidation = validateFile($_FILES['fpic'], $allowedImageTypes, $maxFileSize);
+    $doc1Validation = validateFile($_FILES['ftndoc'], $allowedDocumentTypes, $maxFileSize);
+    $doc2Validation = validateFile($_FILES['ftcdoc'], $allowedDocumentTypes, $maxFileSize);
+    $doc3Validation = validateFile($_FILES['fdmdoc'], $allowedDocumentTypes, $maxFileSize);
+    $doc4Validation = validateFile($_FILES['fdcdoc'], $allowedDocumentTypes, $maxFileSize);
+    $proof1Validation = validateFile($_FILES['fide'], $allowedProofTypes, $maxFileSize);
+    $proof2Validation = validateFile($_FILES['fsig'], $allowedProofTypes, $maxFileSize);
+
+    if ($picValidation || $doc1Validation || $doc2Validation || $doc3Validation || $doc4Validation || $proof1Validation || $proof2Validation) {
+        echo $picValidation . " " . $doc1Validation . " " . $doc2Validation . " " . $doc3Validation . " " . $doc4Validation . " " . $proof1Validation . " " . $proof2Validation;
+        exit;
+    }
+
+    $picpath = $picpath . $_FILES['fpic']['name'];
+    $docpath1 = $docpath . $_FILES['ftndoc']['name'];
+    $docpath2 = $docpath . $_FILES['ftcdoc']['name'];
+    $docpath3 = $docpath . $_FILES['fdmdoc']['name'];
+    $docpath4 = $docpath . $_FILES['fdcdoc']['name'];
+    $proofpath1 = $proofpath . $_FILES['fide']['name'];
+    $proofpath2 = $proofpath . $_FILES['fsig']['name'];
+
+    // Check if the files were uploaded successfully
+    if (move_uploaded_file($_FILES['fpic']['tmp_name'], $picpath) &&
+    move_uploaded_file($_FILES['ftndoc']['tmp_name'], $docpath1) &&
+    move_uploaded_file($_FILES['ftcdoc']['tmp_name'], $docpath2) &&
+    move_uploaded_file($_FILES['fdmdoc']['tmp_name'], $docpath3) &&
+    move_uploaded_file($_FILES['fdcdoc']['tmp_name'], $docpath4) &&
+    move_uploaded_file($_FILES['fide']['tmp_name'], $proofpath1) &&
+    move_uploaded_file($_FILES['fsig']['tmp_name'], $proofpath2))
     {
-        echo "Error <br/>".$sp->error;        
+        // Update the user's profile with the file paths
+        $query = "UPDATE student SET picpath='$picpath', tndocpath='$docpath1', tcdocpath='$docpath2', dmdocpath='$docpath3', dcdocpath='$docpath4', idproofpath='$proofpath1', sigproofpath='$proofpath2' WHERE id='$id'";
+        $result = $sp->query($query);
+        if ($result) {
+            echo "Files uploaded successfully";
+        } else {
+            echo "Error uploading files";
+        }
+    } else
+    {
+        echo "Error uploading files";
     }
 }
-else
-{
-echo "There is an error,please retry or ckeck path";
-}
-}
- ?>
+?>
